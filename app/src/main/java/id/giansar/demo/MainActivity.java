39874b919ccd8e25d9b46f93b94167f36f9a972e
Giansar Pratiknya <gpratiknya@gmail.com>
2021-09-27 00:37:41 +0700

    adding fragments

--- app/src/main/java/id/giansar/demo/MainActivity.java
+++ app/src/main/java/id/giansar/demo/MainActivity.java
@@ -1,10 +1,12 @@
 package id.giansar.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
@@ -15,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(listenerNavigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_activity_main, new HomeFragment()).commit();
     }
+
+    private NavigationBarView.OnItemSelectedListener listenerNavigation = item -> {
+        Fragment selectFragment = null;
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                selectFragment = new HomeFragment();
+                break;
+            case R.id.menu_form:
+                selectFragment = new FormFragment();
+                break;
+            case R.id.menu_quota:
+                selectFragment = new QuotaFragment();
+                break;
+            case R.id.menu_account:
+                selectFragment = new AccountFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_activity_main, selectFragment).commit();
+        return true;
+    };
 }
